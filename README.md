7882f4d041a4cbaef63e9abac7593edb33f48be3
a <a@a.com>
2019-11-22 21:56:18 +0800

    good

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿= Learning JavaScript, 3rd Edition
+== Chapter x: JavaScript Development Tools
+
+In this chapter we're learning about .....
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿const  gulp = require('gulp');
+const  babel = require('gulp-babel');
+const  eslint = require('gulp-eslint'); 
+//var gulpDebug = require('gulp-debug');
+
+gulp.task('default', async function() {
+ //執行Eslint
+  gulp.src(["es6/**/*.js","public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+ //node 來源
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+    //browser 來源
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+  });
